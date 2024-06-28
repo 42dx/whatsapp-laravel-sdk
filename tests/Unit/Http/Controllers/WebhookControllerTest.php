ebc7865e22f922e370aae1c7849c9e49c9ec7cc3
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2024-06-28 19:25:39 -0300

    test(controller): added test scaffold [#0]

---
+++ tests/Unit/Http/Controllers/WebhookControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace The42dx\Whatsapp\Tests\Unit\Http\Controllers;
+
+use The42dx\Whatsapp\Tests\Unit\UnitTestCase;
+
+class WebhookControllerTest extends UnitTestCase {
+    public function test_itShouldDoSomething() {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/UnitTestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace The42dx\Whatsapp\Tests\Unit;
+
+use Orchestra\Testbench\TestCase;
+
+class UnitTestCase extends TestCase {
+    protected function setUp(): void {
+        parent::setUp();
+    }
+}
