8d01b3ef896de82c01de294371be31d127fdb709
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2024-07-09 15:02:47 -0300

    test(entity): added unit tests for changes entities [#0]

--- src/WhatsappServiceProvider.php
+++ src/WhatsappServiceProvider.php
@@ -5,12 +5,14 @@ namespace The42dx\Whatsapp;
 use Illuminate\Support\ServiceProvider;
 
 class WhatsappServiceProvider extends ServiceProvider {
+    const SERVICE_NAME = 'whatsapp';
+
     public function register() {
-        $this->app->bind('whatsapp', function () {
+        $this->app->bind(self::SERVICE_NAME, function () {
             // return new Whatsapp();
         });
 
-        $this->mergeConfigFrom(__DIR__ . '/../config/whatsapp.php', 'whatsapp');
+        $this->mergeConfigFrom(__DIR__ . '/../config/'. self::SERVICE_NAME . '.php', self::SERVICE_NAME);
     }
 
     public function boot() {
---
+++ tests/Unit/Entities/Changes/ContactsEntityTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use The42dx\Whatsapp\Contracts\Entity;
+use The42dx\Whatsapp\Entities\Changes\ContactsEntity;
+use The42dx\Whatsapp\Tests\Unit\UnitTestCase;
+
+class ContactsEntityTest extends UnitTestCase {
+    public function test_itShouldBeAnEntityInstanceObject() {
+        $changes = new ContactsEntity([]);
+
+        $this->assertIsObject($changes);
+        $this->assertTrue($changes instanceof Entity);
+    }
+
+    public function test_itShouldCreateObjectWithCorrectAttributes() {
+        $contactData = $this->getJsonFixture('Api/Components/change-contact');
+        $contact     = new ContactsEntity($contactData);
+
+        $this->assertIsObject($contact);
+
+        $this->assertNotNull($contact->name);
+        $this->assertEquals('Some Name And Surname', $contact->name);
+
+        $this->assertNotNull($contact->waId);
+        $this->assertEquals('123123123123', $contact->waId);
+    }
+
+    public function test_itShouldUpdateAttributes() {
+        $expectedName = 'Some Interesting Name';
+        $expectedWaId = '098876654';
+        $contact = new ContactsEntity([]);
+
+        $this->assertNull($contact->name);
+        $this->assertNull($contact->waId);
+
+        $contact->setAttributes([
+            'profile' => [
+                'name' => $expectedName
+            ],
+            'wa_id'   => $expectedWaId,
+        ]);
+
+        $this->assertNotNull($contact->name);
+        $this->assertEquals($expectedName, $contact->name);
+
+        $this->assertNotNull($contact->waId);
+        $this->assertEquals($expectedWaId, $contact->waId);
+    }
+}
