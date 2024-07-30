48b162b9f0e63d108b9d6f922cb93f1265543d99
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2024-07-30 20:13:23 -0300

    feat(message): implemented text message sending [#5]

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use The42dx\Whatsapp\Enums\MessageType;
 use The42dx\Whatsapp\Http\Controllers\WebhookController;
 
 $webhookRoute = config('whatsapp.webhook_route', 'webhook/whatsapp');
 
 Route::get($webhookRoute, [WebhookController::class, 'check']);
 Route::post($webhookRoute, [WebhookController::class, 'handle']);
+
+Route::get('/asd', function() {
+    User::find(1)->sendWhatsappMsg(MessageType::TEXT, 'Test Yeah!');
+});
--- samples/laravel-11/app/Models/User.php
+++ samples/laravel-11/app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use The42dx\Whatsapp\Models\Traits\CanSendWhatsappMsg;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, CanSendWhatsappMsg;
 
     /**
      * The attributes that are mass assignable.
