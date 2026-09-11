30a556d41469c95649f2aebe1ca6b0c94a69d688
Rafael Eduardo Paulin <rafapaulin@gmail.com>
2026-09-11 17:42:00 -0300

    chore(sample): added notification example [#0]
    Add a sample WhatsApp notification and route for exercising delivery
    from the Laravel 12 application.

--- samples/laravel-12/routes/web.php
+++ samples/laravel-12/routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Models\User;
+use App\Notifications\Not;
 use Illuminate\Support\Facades\Route;
+use The42dx\Whatsapp\Enums\MessageType;
+
 
 Route::get('/', fn () => view('welcome'));
+Route::get('/asd', function () {
+    /** @var User $user */
+    $user = User::first();
+
+    $user->notify(new Not());
+});
